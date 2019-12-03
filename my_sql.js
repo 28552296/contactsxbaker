33a3bdd38c8c55d9065d43c5bbce8d009ccc3f1b
Robin Molelemane <57193655+molelemane@users.noreply.github.com>
2019-12-03 16:37:02 +0200

    Create my_sql.js

---
+++ my_sql.js
@@ -0,0 +1,16 @@
+const MYSQL = require('mysql'); 
+
+const SQL_CONNECTION = MYSQL.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'password',
+    database:'contactsdb'
+});
+
+SQL_CONNECTION.connect(function(error){
+    if(!!error) console.log(error);
+    else console.log('Database Connected!');
+
+}); 
+
+module.exports = SQL_CONNECTION //Export my_sql connection to use in index.js
