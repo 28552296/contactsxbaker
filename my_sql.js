c582f504bf7d2efe124b2627ea9a37f687b34cbf
Robin Molelemane <57193655+molelemane@users.noreply.github.com>
2019-12-03 18:22:09 +0200

    Update my_sql.js
    See line 7.

--- my_sql.js
+++ my_sql.js
@@ -4,7 +4,7 @@ const SQL_CONNECTION = MYSQL.createConnection({
     host:'localhost',
     user:'root',
     password:'password',
-    database:'contactsdb'
+    database:'contactlistdb'
 });
 
 SQL_CONNECTION.connect(function(error){
